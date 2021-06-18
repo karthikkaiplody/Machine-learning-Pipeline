0e3bf5b4b5210d413062e703a359ce197d65973f
KarthikKaiplody <logontokarthikk@gmail.com>
2021-06-18 18:27:21 +0530

    First commit: Folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project pipline built with MLFlow and Pycaret',
+    author='KarthikKaiplody',
+    license='MIT',
+)
---
+++ src/features/helper_features.py
@@ -0,0 +1,33 @@
+# Standard helper functions for structured data
+
+# importing the libraries
+# Standard
+import numpy as np
+import pandas as pd
+
+# Pycaret
+from pycaret.classification import *
+
+# Plots
+from plotly.offline import iplot
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import seaborn as sns
+
+
+# Getting the dataset info
+def dataset_info(dataset, dataset_name:str ):
+    print(f"Dataser Name:{dataset_name} | Number of samples: {dataset.shape[0]} | Number of columns: {dataset.shape[1]}")
+    print(30*"=")
+    print("Column           Data Type")
+    print(dataset.dtypes)
+    print(30*"=")
+    missing_data = dataset.isnull().sum()
+    if sum(missing_data) > 0:
+        print(missing_data[missing_data.data.values > 0])
+    else:
+        print("Looks like there is no missing Data!!")
+    print(30*"=")
+    print(f"Memory Usage: {np.round(dataset.memory_usage(index=True).sum() / 10e5, 3)} MB") 
+
